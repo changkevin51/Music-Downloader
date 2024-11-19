dd80053f94dae03df7207bbfba673d001834e4cb
Kevin Chang <changkevin51@gmail.com>
2024-11-18 23:18:11 -0500

    small fixes

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import subprocess
-
+service = Service(ChromeDriverManager().install())
 # Suppress warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
